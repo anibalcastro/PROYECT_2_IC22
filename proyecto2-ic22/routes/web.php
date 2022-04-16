d7c88716b141e4c5bc6315860a34449bb5ed7e96
anibalcastro <anibalcastro1515@gmail.com>
2022-04-16 12:57:31 -0600

    comment routes

--- proyecto2-ic22/routes/web.php
+++ proyecto2-ic22/routes/web.php
@@ -28,38 +28,18 @@ Route::get('admin/createCategory',[AdminController::class,'create']);
 //Route::resource('user', UserController::class);
 //Route::resource('admin', AdminController::class);
 
-//Admin
-//Dashboard
+//Admin \ Dashboard \ Create category \ Logout \ Edit \ Save category \ Update \ Delete
+
+
 Route::get('admin', [AdminController::class, 'index'])-> name('Dashboard-Admin / N-Noticias');
-//Create category
 Route::get('admin/create', [AdminController::class,'create'])-> name('Create - Category / N-Noticias');
-//Logout
 Route::get('exit', [AdminController::class, 'exit']);
-/*
-Route::get('admin/exit', function () {
-    session()->forget(['email', 'firstName', 'roleId']);
-    $data['pageTitle'] = 'Login / N-Noticias';
-    return view('user.login', $data);
-});
-*/
-//Edit
 Route::get('admin/{admin}/edit', [AdminController::class, 'edit'])->name('Edit Categorie / N-Noticias');
-//Save category
 Route::post('admin', [AdminController::class, 'store']);
-//Update 
 Route::patch('admin/{admin}', [AdminController::class, 'update']);
-//Delete
 Route::delete('admin/{admin}', [AdminController::class, 'destroy']);
 
-//POST admin
-//GET admin/{admin}/edit
-//PATCH admin/{admin}
-//DELETE admin/{admin}
-//Route::resource('admin', AdminController::class);
-
-
-
-//User
+//User \ Register \ login \ validateLogin \ registerAction
 Route::get('register', [UserController::class, 'register'])-> name('Registro / N-Noticias');
 Route::get('login', [UserController::class, 'index'])-> name('Login / N-Noticias');
 Route::get('/', [UserController::class, 'index'])-> name('Login / N-Noticias');
