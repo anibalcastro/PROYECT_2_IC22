edcf22d49fa3f1d3b1561d87f2e4902b2fa9defb
anibalcastro <anibalcastro1515@gmail.com>
2022-04-15 22:26:34 -0600

    update

--- proyecto2-ic22/public/js/shared.js
+++ proyecto2-ic22/public/js/shared.js
@@ -1,4 +1,11 @@
+
+//Ocultar la alerta generada por session
 function ocultarAlerta(){
     var x = document.getElementById("alerta");
     x.style.display = "none";
 }
+
+function minusculas(e){
+    e.value = e.value.toLowerCase();
+}
+
