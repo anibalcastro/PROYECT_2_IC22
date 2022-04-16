a938d2073908448c9cc3832aef3db7a183204785
anibalcastro <anibalcastro1515@gmail.com>
2022-04-16 13:37:41 -0600

    create views source

---
+++ proyecto2-ic22/resources/views/source/createSource.blade.php
@@ -0,0 +1,14 @@
+<!--Contenido del menu-->
+<?=$head?>
+<body>
+    <!--Contenido del menu-->
+    <?=$menu?>
+    <!--Contenido del encabezado-->
+
+    <!--Contenido del formulario-->
+    <form action="" method="post">
+        
+
+    </form>
+    @include('share/footer');
+</body>
---
+++ proyecto2-ic22/resources/views/source/dashboard.blade.php
@@ -0,0 +1,23 @@
+<?=$head?>
+<body>
+    <?=$menu?>
+    
+    <?php
+    if(Session::has('message')){?>
+    <div id="alerta" class="alert alert-success alert-dismissible" role="alert">
+        <button type="button" onclick="ocultarAlerta()" class="close" data-dismiss="alert" aria-label="Close"><span
+                aria-hidden="true">&times;</span></button>
+        <?= Session::get('message');?>
+    </div>
+    <?php }?>
+
+    <div class="jumbotron">
+        <h1 class="display-4">Your unique news</h1>
+        <div class="linea_100"></div>
+    </div>
+
+    <!--Menu de todas las categorias->
+    
+
+    @include('share/footer');
+</body>
